b01710dd682a57fc6a02328f5e28156aee9f2379
Pedro Henrique <pedro.henrique59@outlook.com>
2023-01-23 18:45:37 -0300

    Implementando rest templaate.

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,51 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private Double dailyInCome;
+
+    public Worker() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyInCome() {
+        return dailyInCome;
+    }
+
+    public void setDailyInCome(Double dailyInCome) {
+        this.dailyInCome = dailyInCome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(id, worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

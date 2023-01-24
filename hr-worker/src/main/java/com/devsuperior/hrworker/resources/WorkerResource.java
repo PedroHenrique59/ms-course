a6705ee86391c7d117784718736b4c35867f7e1b
Pedro Henrique <pedro.henrique59@outlook.com>
2023-01-24 17:37:58 -0300

    Implementando Ribbon Balance.

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import java.util.List;
 public class WorkerResource {
 
     @Autowired
+    private Environment env;
+
+    private Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
     private WorkerRepository workerRepository;
 
     @GetMapping
@@ -25,6 +33,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("Port = " + env.getProperty("local.server.port"));
+
         return ResponseEntity.ok(workerRepository.findById(id).get());
     }
 
